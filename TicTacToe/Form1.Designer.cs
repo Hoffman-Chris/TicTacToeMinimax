47383b72be24de090a3d1be8313ce44404b8ce0e
Chris Hoffman <chrishoff143@gmail.com>
2021-01-04 15:41:29 -0600

    initial commit

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.A0 = new System.Windows.Forms.Button();
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.B0 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.C0 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(499, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 26);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 26);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // A0
+            // 
+            this.A0.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A0.Location = new System.Drawing.Point(16, 31);
+            this.A0.Name = "A0";
+            this.A0.Size = new System.Drawing.Size(150, 150);
+            this.A0.TabIndex = 1;
+            this.A0.UseVisualStyleBackColor = true;
+            this.A0.Click += new System.EventHandler(this.button_click);
+            // 
+            // A1
+            // 
+            this.A1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A1.Location = new System.Drawing.Point(172, 31);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(150, 150);
+            this.A1.TabIndex = 2;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.button_click);
+            // 
+            // A2
+            // 
+            this.A2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A2.Location = new System.Drawing.Point(328, 31);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(150, 150);
+            this.A2.TabIndex = 3;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.button_click);
+            // 
+            // B0
+            // 
+            this.B0.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B0.Location = new System.Drawing.Point(16, 187);
+            this.B0.Name = "B0";
+            this.B0.Size = new System.Drawing.Size(150, 150);
+            this.B0.TabIndex = 4;
+            this.B0.UseVisualStyleBackColor = true;
+            this.B0.Click += new System.EventHandler(this.button_click);
+            // 
+            // B1
+            // 
+            this.B1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B1.Location = new System.Drawing.Point(172, 187);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(150, 150);
+            this.B1.TabIndex = 5;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.button_click);
+            // 
+            // B2
+            // 
+            this.B2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B2.Location = new System.Drawing.Point(328, 187);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(150, 150);
+            this.B2.TabIndex = 6;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.button_click);
+            // 
+            // C0
+            // 
+            this.C0.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C0.Location = new System.Drawing.Point(16, 343);
+            this.C0.Name = "C0";
+            this.C0.Size = new System.Drawing.Size(150, 150);
+            this.C0.TabIndex = 7;
+            this.C0.UseVisualStyleBackColor = true;
+            this.C0.Click += new System.EventHandler(this.button_click);
+            // 
+            // C1
+            // 
+            this.C1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1.Location = new System.Drawing.Point(172, 343);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(150, 150);
+            this.C1.TabIndex = 8;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.button_click);
+            // 
+            // C2
+            // 
+            this.C2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2.Location = new System.Drawing.Point(328, 343);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(150, 150);
+            this.C2.TabIndex = 9;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.button_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 516);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.C0);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.B0);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.A0);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button A0;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button B0;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button C0;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true; // true = X turn (human player), false = Y turn (AI player)
+        int turnCount = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Chris", "Tic Tac Toe About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /*
+         * For when the human player clicks a button to take their turn.
+         * AI makes it's move in response to human player turn as long as there has not been a winner.
+         */
+        private void button_click(object sender, EventArgs e)
+        {
+            Button buttonClicked = (Button)sender;
+            buttonClicked.Enabled = false;
+            buttonClicked.Text = "O";
+
+            turnCount++;
+            turn = true;
+
+            // AI makes it's move in response
+            if (!checkForWinner())
+            {
+                moveAI();
+                turn = false;
+                turnCount++;
+                checkForWinner();
+            }
+
+        }
+
+        /*
+         * Checks the board to see if there has been a winner and, if so, displays a message.
+         */
+        private bool checkForWinner()
+        {
+
+            bool winner = false;
+
+            // horizontal check
+            if (A0.Text == A1.Text && A1.Text == A2.Text && !A0.Enabled)
+                winner = true;
+            if (B0.Text == B1.Text && B1.Text == B2.Text && !B0.Enabled)
+                winner = true;
+            if (C0.Text == C1.Text && C1.Text == C2.Text && !C0.Enabled)
+                winner = true;
+
+            // vertical check
+            if (A0.Text == B0.Text && B0.Text == C0.Text && !A0.Enabled)
+                winner = true;
+            if (A1.Text == B1.Text && B1.Text == C1.Text && !A1.Enabled)
+                winner = true;
+            if (A2.Text == B2.Text && B2.Text == C2.Text && !A2.Enabled)
+                winner = true;
+
+            // diagonal check
+            if (A0.Text == B1.Text && B1.Text == C2.Text && !A0.Enabled)
+                winner = true;
+            if (A2.Text == B1.Text && B1.Text == C0.Text && !A2.Enabled)
+                winner = true;
+
+            if (winner)
+            {
+                disableButtons();
+                String winningPlayer = "";
+                if (turn)
+                    winningPlayer = "O";
+                else
+                    winningPlayer = "X";
+
+                MessageBox.Show(winningPlayer + " has won the game.");
+            }
+            else
+            {
+                if (turnCount == 9)
+                    MessageBox.Show("It was a tie.");
+            }
+
+            return winner;
+
+        }
+
+        /*
+         * Disables all buttons on the tic tac toe board.
+         */
+        private void disableButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }
+            catch { }
+
+        }
+
+        /*
+         * Starts a new game from scratch.
+         */
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turnCount = 0;
+            turn = true;
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }
+            }
+            catch { }
+        }
+
+        // Minimax
+
+        /*
+         * Private class Move that represents a move the AI can make.
+         * Stores the index of the possible move and the score associated with that move.
+         */
+        private class Move
+        {
+            private int index;
+            private int score;
+
+            public Move(int index, int score)
+            {
+                this.index = index;
+                this.score = score;
+            }
+
+            public void setIndex( int index)
+            {
+                this.index = index;
+            }
+
+            public void setScore( int score)
+            {
+                this.score = score;
+            }
+
+            public int getIndex()
+            {
+                return index;
+            }
+
+            public int getScore()
+            {
+                return score;
+            }
+        }
+
+        /*
+         * Takes the current game board state, calculates the best possible move, and makes a move accordingly.
+         */
+        private void moveAI()
+        {
+            String[] originalBoard = { A0.Text, A1.Text, A2.Text, B0.Text, B1.Text, B2.Text, C0.Text, C1.Text, C2.Text };
+            int moveIndex;
+            if( turnCount == 1 && B1.Text == "" )
+            {
+                moveIndex = 4;
+            }
+            else
+            {
+                moveIndex = minimax(originalBoard, "X", 0).getIndex();
+            }
+            
+            if (moveIndex == 0)
+            {
+                A0.Text = "X";
+                A0.Enabled = false;
+            }
+            else if (moveIndex == 1)
+            {
+                A1.Text = "X";
+                A1.Enabled = false;
+            }
+            else if (moveIndex == 2)
+            {
+                A2.Text = "X";
+                A2.Enabled = false;
+            }
+            else if (moveIndex == 3)
+            {
+                B0.Text = "X";
+                B0.Enabled = false;
+            }
+            else if (moveIndex == 4)
+            {
+                B1.Text = "X";
+                B1.Enabled = false;
+            }
+            else if (moveIndex == 5)
+            {
+                B2.Text = "X";
+                B2.Enabled = false;
+            }
+            else if (moveIndex == 6)
+            {
+                C0.Text = "X";
+                C0.Enabled = false;
+            }
+            else if (moveIndex == 7)
+            {
+                C1.Text = "X";
+                C1.Enabled = false;
+            }
+            else if (moveIndex == 8)
+            {
+                C2.Text = "X";
+                C2.Enabled = false;
+            }
+
+        }
+
+        /*
+         * Implementation of minimax algorithm for calculating best possible move.
+         */
+        private Move minimax(String[] board, String player, int depth)
+        {
+            String human = "O";
+            String ai = "X";
+            ArrayList availableSpots = emptyIndexes(board);
+
+            if (winning(board, human))
+            {
+                return new Move(-1, -10 + depth);
+            }
+            else if (winning(board, ai))
+            {
+                return new Move(-1, 10 - depth);
+            }
+            else if (availableSpots.Count == 0)
+            {
+                return new Move(-1, 0);
+            }
+
+            Move[] moves = new Move[availableSpots.Count];
+
+            for( int i = 0; i < availableSpots.Count; i++)
+            {
+                Move move = new Move((int)availableSpots[i], -1);
+
+                board[(int)availableSpots[i]] = player;
+
+                if (player == ai)
+                {
+                    move.setScore(minimax(board, human, depth++).getScore());
+                }
+                else
+                {
+                    move.setScore(minimax(board, ai, depth++).getScore());
+                }
+
+                board[(int)availableSpots[i]] = "";
+
+                moves[i] = move;
+            }
+
+            int bestMove = -1;
+            if (player == ai)
+            {
+                int bestScore = -99999;
+                for (int i = 0; i < moves.Length; i++)
+                {
+                    if (moves[i].getScore() > bestScore)
+                    {
+                        bestScore = moves[i].getScore();
+                        bestMove = i;
+                    }
+                }
+            }
+            else
+            {
+                int bestScore = 999999;
+                for (int i = 0; i < moves.Length; i++)
+                {
+                    if(moves[i].getScore() < bestScore)
+                    {
+                        bestScore = moves[i].getScore();
+                        bestMove = i;
+                    }
+                }
+            }
+            
+            return moves[bestMove];
+        }
+
+       
+        /*
+         * Finds all empty indexes on the board that the AI could possibly make a move to.
+         */
+        private ArrayList emptyIndexes(String[] board)
+        {
+            ArrayList freeSpaces = new ArrayList();
+            for (int i = 0; i < board.Length; i++)
+            {
+                if (board[i] == "")
+                    freeSpaces.Add(i);
+            }
+
+            return freeSpaces;
+        }
+
+        /*
+         * Checks if the state of the board is a winning state.
+         * Used in minimax algorithm to assign score values to moves.
+         */
+        private bool winning( String[] board, String player )
+        {
+            // horizontal check
+            if (board[0] == player && board[1] == player && board[2] == player)
+                return true;
+            if (board[3] == player && board[4] == player && board[5] == player)
+                return true;
+            if (board[6] == player && board[7] == player && board[8] == player)
+                return true;
+
+            // vertical check
+            if (board[0] == player && board[3] == player && board[6] == player)
+                return true;
+            if (board[1] == player && board[4] == player && board[7] == player)
+                return true;
+            if (board[2] == player && board[5] == player && board[8] == player)
+                return true;
+
+            // diagonal check
+            if (board[0] == player && board[4] == player && board[8] == player)
+                return true;
+            if (board[2] == player && board[4] == player && board[6] == player)
+                return true;
+
+            // loss
+            return false;
+        }
+    }
+}
